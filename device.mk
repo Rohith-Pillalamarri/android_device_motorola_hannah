ba0ccb4363acfcbf8a96b4d8d9d918fd24a1848e
Asura <53406857+Rohith-Pillalamarri@users.noreply.github.com>
2020-05-08 09:37:30 +0530

    Update device.mk

--- device.mk
+++ device.mk
@@ -33,6 +33,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/camera/ov5675_jeterht_chromatix.xml:$(TARGET_COPY_OUT_VENDOR)/etc/camera/ov5675_jeterht_chromatix.xml \
     $(LOCAL_PATH)/configs/camera/ov5675_jeterkc_chromatix.xml:$(TARGET_COPY_OUT_VENDOR)/etc/camera/ov5675_jeterkc_chromatix.xml
 
+
+# Maintainer
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.device.maintainer=Rohith Pillalamarri
+
 # Ramdisk
 PRODUCT_PACKAGES += \
     init.device.rc
