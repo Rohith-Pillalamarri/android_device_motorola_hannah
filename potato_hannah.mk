7d2efd71a2ab0f7fbe5653a253d436741f5835c0
Asura <53406857+Rohith-Pillalamarri@users.noreply.github.com>
2020-05-12 19:38:13 +0530

    Update and rename bootleg_hannah.mk to potato_hannah.mk

--- potato_hannah.mk
+++ potato_hannah.mk
@@ -11,11 +11,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/bootleg/config/common_full_phone.mk)
+$(call inherit-product, vendor/potato/config/common_full_phone.mk)
 TARGET_GAPPS_ARCH := arm64
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := bootleggers_hannah
+PRODUCT_NAME := potato_hannah
 PRODUCT_DEVICE := hannah
 PRODUCT_BRAND := motorola
 PRODUCT_MODEL := moto e5 plus
